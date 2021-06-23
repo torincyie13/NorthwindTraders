4bfa33c509d9204699d9aa94f01ae41c2656e882
torincyie13 <85264448+torincyie13@users.noreply.github.com>
2021-06-23 16:34:07 -0500

    Add files via upload

---
+++ NorthwindTraders-master (1)/NorthwindTraders-master/NorthwindTraders/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿
+namespace NorthwindTraders
+{
+    partial class frmCustomers
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtCustomerID = new System.Windows.Forms.TextBox();
+            this.txtCompanyName = new System.Windows.Forms.TextBox();
+            this.txtContactName = new System.Windows.Forms.TextBox();
+            this.txtContactTitle = new System.Windows.Forms.TextBox();
+            this.btnFirst = new System.Windows.Forms.Button();
+            this.btnPrevious = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnLast = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(89, 60);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Customer ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(89, 159);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Company Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(89, 250);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Contact Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(89, 356);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Contact Title";
+            // 
+            // txtCustomerID
+            // 
+            this.txtCustomerID.BackColor = System.Drawing.Color.White;
+            this.txtCustomerID.Location = new System.Drawing.Point(249, 38);
+            this.txtCustomerID.Margin = new System.Windows.Forms.Padding(4);
+            this.txtCustomerID.Multiline = true;
+            this.txtCustomerID.Name = "txtCustomerID";
+            this.txtCustomerID.ReadOnly = true;
+            this.txtCustomerID.Size = new System.Drawing.Size(593, 77);
+            this.txtCustomerID.TabIndex = 4;
+            // 
+            // txtCompanyName
+            // 
+            this.txtCompanyName.BackColor = System.Drawing.Color.White;
+            this.txtCompanyName.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtCompanyName.Location = new System.Drawing.Point(249, 133);
+            this.txtCompanyName.Margin = new System.Windows.Forms.Padding(4);
+            this.txtCompanyName.Multiline = true;
+            this.txtCompanyName.Name = "txtCompanyName";
+            this.txtCompanyName.ReadOnly = true;
+            this.txtCompanyName.Size = new System.Drawing.Size(593, 77);
+            this.txtCompanyName.TabIndex = 5;
+            // 
+            // txtContactName
+            // 
+            this.txtContactName.BackColor = System.Drawing.Color.White;
+            this.txtContactName.Location = new System.Drawing.Point(249, 229);
+            this.txtContactName.Margin = new System.Windows.Forms.Padding(4);
+            this.txtContactName.Multiline = true;
+            this.txtContactName.Name = "txtContactName";
+            this.txtContactName.ReadOnly = true;
+            this.txtContactName.Size = new System.Drawing.Size(593, 77);
+            this.txtContactName.TabIndex = 6;
+            // 
+            // txtContactTitle
+            // 
+            this.txtContactTitle.BackColor = System.Drawing.Color.White;
+            this.txtContactTitle.Location = new System.Drawing.Point(249, 327);
+            this.txtContactTitle.Margin = new System.Windows.Forms.Padding(4);
+            this.txtContactTitle.Multiline = true;
+            this.txtContactTitle.Name = "txtContactTitle";
+            this.txtContactTitle.ReadOnly = true;
+            this.txtContactTitle.Size = new System.Drawing.Size(593, 77);
+            this.txtContactTitle.TabIndex = 7;
+            // 
+            // btnFirst
+            // 
+            this.btnFirst.Location = new System.Drawing.Point(163, 471);
+            this.btnFirst.Margin = new System.Windows.Forms.Padding(4);
+            this.btnFirst.Name = "btnFirst";
+            this.btnFirst.Size = new System.Drawing.Size(100, 28);
+            this.btnFirst.TabIndex = 8;
+            this.btnFirst.Text = "&First";
+            this.btnFirst.UseVisualStyleBackColor = true;
+            this.btnFirst.Click += new System.EventHandler(this.btnFirst_Click);
+            // 
+            // btnPrevious
+            // 
+            this.btnPrevious.Location = new System.Drawing.Point(341, 471);
+            this.btnPrevious.Margin = new System.Windows.Forms.Padding(4);
+            this.btnPrevious.Name = "btnPrevious";
+            this.btnPrevious.Size = new System.Drawing.Size(100, 28);
+            this.btnPrevious.TabIndex = 9;
+            this.btnPrevious.Text = "&Previous";
+            this.btnPrevious.UseVisualStyleBackColor = true;
+            this.btnPrevious.Click += new System.EventHandler(this.btnPrevious_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(523, 471);
+            this.btnNext.Margin = new System.Windows.Forms.Padding(4);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(100, 28);
+            this.btnNext.TabIndex = 10;
+            this.btnNext.Text = "&Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnLast
+            // 
+            this.btnLast.Location = new System.Drawing.Point(703, 471);
+            this.btnLast.Margin = new System.Windows.Forms.Padding(4);
+            this.btnLast.Name = "btnLast";
+            this.btnLast.Size = new System.Drawing.Size(100, 28);
+            this.btnLast.TabIndex = 11;
+            this.btnLast.Text = "&Last";
+            this.btnLast.UseVisualStyleBackColor = true;
+            this.btnLast.Click += new System.EventHandler(this.btnLast_Click);
+            // 
+            // frmCustomers
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(912, 539);
+            this.Controls.Add(this.btnLast);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnPrevious);
+            this.Controls.Add(this.btnFirst);
+            this.Controls.Add(this.txtContactTitle);
+            this.Controls.Add(this.txtContactName);
+            this.Controls.Add(this.txtCompanyName);
+            this.Controls.Add(this.txtCustomerID);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "frmCustomers";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Customers Databases";
+            this.Load += new System.EventHandler(this.frmCustomers_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtCustomerID;
+        private System.Windows.Forms.TextBox txtCompanyName;
+        private System.Windows.Forms.TextBox txtContactName;
+        private System.Windows.Forms.TextBox txtContactTitle;
+        private System.Windows.Forms.Button btnFirst;
+        private System.Windows.Forms.Button btnPrevious;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnLast;
+    }
+}
+
---
+++ NorthwindTraders-master (1)/NorthwindTraders-master/NorthwindTraders/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace NorthwindTraders
+{
+    public partial class frmCustomers : Form
+    {
+        public frmCustomers()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection NWindConnection;
+        SqlCommand customersCommand;
+        SqlDataAdapter customersAdapter;
+        DataTable customersTable;
+        CurrencyManager customersManager;
+
+        private void frmCustomers_Load(object sender, EventArgs e)
+        {
+            // string path = Path.GetFullPath("SQLNWindDB.mdf");
+            // MessageBox.Show(path);
+            // connect to NWind database
+            NWindConnection = new
+                SqlConnection("Data Source=.\\SQLEXPRESS01; AttachDbFilename=C:\\Users\\sogli\\Downloads\\NorthwindTraders-master (1)\\NorthwindTraders-master\\SQLNwindDB.mdf; Integrated Security=True; Connect Timeout=30; User Instance=True");
+            // open the connection
+            NWindConnection.Open();
+            // establish command object
+            customersCommand = new SqlCommand("SELECT * FROM Customers", NWindConnection);
+            // establish data adapter/data table
+            customersAdapter = new SqlDataAdapter();
+            customersAdapter.SelectCommand = customersCommand;
+            customersTable = new DataTable();
+            customersAdapter.Fill(customersTable);
+            // bind controls to data table
+            txtCustomerID.DataBindings.Add("Text", customersTable, "CustomerID");
+            txtCompanyName.DataBindings.Add("Text", customersTable, "CompanyName");
+            txtContactName.DataBindings.Add("Text", customersTable, "ContactName");
+            txtContactTitle.DataBindings.Add("Text", customersTable, "ContactTitle");
+            // establish currecny manager
+            customersManager = (CurrencyManager)
+                BindingContext[customersTable];
+            // close connection
+            NWindConnection.Close();
+            // dispose of the conneciton object
+            NWindConnection.Dispose();
+            customersCommand.Dispose();
+            customersAdapter.Dispose();
+            customersTable.Dispose();
+        }
+
+        private void btnFirst_Click(object sender, EventArgs e)
+        {
+            customersManager.Position = 0;
+        }
+
+        private void btnPrevious_Click(object sender, EventArgs e)
+        {
+            customersManager.Position--;
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            customersManager.Position++;
+        }
+
+        private void btnLast_Click(object sender, EventArgs e)
+        {
+            customersManager.Position = customersManager.Count - 1;
+        }
+    }
+}
